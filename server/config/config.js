1a238dc52a248d66d1e90c46250bb81c3a3803d9
daniel parra <danialepaco@hotmail.com>
2020-02-12 09:17:20 -0500

    login Added

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,38 @@
-//================
-//Puerto
-//================
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
 
-process.env.PORT = process.env.PORT || 3000
 
-//================
-//Entorno
-//================
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-//================
-//Base de datos
-//================
-let urlDB
+
+// ============================
+//  Vencimiento del Token
+// ============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ============================
+//  SEED de autenticación
+// ============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://danialepaco:0ipqj1WHrY7zftex@cluster0-wfbz5.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
-
-process.env.URLDB = urlDB
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
-require('./config/config')
-const express = require('express')
-const app = express()
+require('./config/config');
+
+const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
+
+
+const app = express();
+
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(require('./routes/usuario'))
 
-mongoose.connect(process.env.URLDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, (err, res) => {
-    if (err) throw err
-    console.log(res)
-    console.log('bbdd online')
+// parse application/json
+app.use(bodyParser.json())
+
+
+// Configuración global de rutas
+app.use(require('./routes/index'));
+
+
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+
 });
 
-app.listen(process.env.PORT)
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
